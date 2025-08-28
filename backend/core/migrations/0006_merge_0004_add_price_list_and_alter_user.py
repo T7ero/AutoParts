2ff9b533d643893241de53104245e0012e59a883
T7ero <tima.cher13@gmail.com>
2025-08-28 14:40:57 +0700

    2nd module: fix error up 6

---
+++ backend/core/migrations/0006_merge_0004_add_price_list_and_alter_user.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0004_add_price_list_models'),
+        ('core', '0004_alter_parsingtask_user'),
+    ]
+
+    operations = [
+    ]
