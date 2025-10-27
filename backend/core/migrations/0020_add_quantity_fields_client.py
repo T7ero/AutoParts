4eefceaf9d042b41043231b0a6e219a9637e12ef
T7ero <tima.cher13@gmail.com>
2025-10-27 23:07:25 +0700

    fix: download file to pricelist 8.11

--- backend/core/migrations/0020_add_quantity_fields_client.py
+++ backend/core/migrations/0020_add_quantity_fields_client.py
@@ -1,13 +1,9 @@
-# Generated manually for client server
-# Добавляет поля для количества товара, если их еще нет
-
 from django.db import migrations, models
-
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0019_remove_parsingtask_log_remove_parsingtask_progress_and_more'),  # Эта миграция есть на сервере заказчика
+        ('core', '0018_add_created_at_to_pricelistitem'),
     ]
 
     operations = [
@@ -19,7 +15,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='pricelistitem',
             name='competitor_quantity',
-            field=models.IntegerField(blank=True, null=True, verbose_name='Количество конкурента'),
+            field=models.IntegerField(blank=True, null=True, verbose_name='Количество у конкурента'),
         ),
     ]
-
